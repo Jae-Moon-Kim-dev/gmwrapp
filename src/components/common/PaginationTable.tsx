9590f7d622322e9abf15e7601f79cd77120c1039
LG_KimJaeMoon <kjmopmjk@gmail.com>
2025-04-25 19:56:18 +0900

    .

--- src/components/common/PaginationTable.tsx
+++ src/components/common/PaginationTable.tsx
@@ -35,13 +35,13 @@ const PaginationTable = <T extends object>( {
         paginationNode.push(<Pagination.First key={'first'} disabled={ currPageSize === 1 } onClick={() => {tanstackTable.firstPage()}} />);
         paginationNode.push(<Pagination.Prev key={'prev'} disabled={ currPageSize === 1 } onClick={() => {tanstackTable.previousPage()}} />);
         paginationNode.push(<Pagination.Item key={`page-1`} active={1 === currPageSize} onClick={() => {tanstackTable.setPageIndex(0)}} >{1}</Pagination.Item>);
-        if ( currPageSize > 5 ) paginationNode.push(<Pagination.Ellipsis key={"ellipsis-start"} disabled={ currPageSize < 6 } onClick={() =>tanstackTable.setPageIndex(currPageSize-3)} />);
+        if ( currPageSize > 5 ) paginationNode.push(<Pagination.Ellipsis key={"ellipsis-start"} disabled={ currPageSize < 6 } onClick={() =>tanstackTable.setPageIndex(currPageSize-4)} />);
 
         for ( let i=(startPageSize-1); i < endPageSize; i++ ) {
             if ( (i+1) > 1 && (i+1) < endPage ) paginationNode.push(<Pagination.Item key={`page-${i+1}`} active={(i+1) === currPageSize} onClick={() => {tanstackTable.setPageIndex(i)}} >{i+1}</Pagination.Item>); 
         }
     
-        if ( currPageSize < (endPage-3)) paginationNode.push(<Pagination.Ellipsis key={"ellipsis-end"} disabled={ currPageSize > endPage-4 } onClick={() =>tanstackTable.setPageIndex(currPageSize+3)} />);
+        if ( currPageSize < (endPage-3)) paginationNode.push(<Pagination.Ellipsis key={"ellipsis-end"} disabled={ currPageSize > endPage-4 } onClick={() =>tanstackTable.setPageIndex(currPageSize+2)} />);
         if ( endPage > 1 ) paginationNode.push(<Pagination.Item key={`page-${endPage}`} active={endPage === currPageSize} onClick={() => {tanstackTable.setPageIndex(endPage-1)}} >{endPage}</Pagination.Item>);
         paginationNode.push(<Pagination.Next key={'next'} disabled={ currPageSize === endPage } onClick={() => {tanstackTable.nextPage()}} />);
         paginationNode.push(<Pagination.Last key={'last'} disabled={ currPageSize === endPage } onClick={() => {tanstackTable.setPageIndex(endPage-1)}} />);
